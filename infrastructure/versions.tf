fb0cbe529c0b24e86eaa4ebc2ec031aead7d3eae
Jim Counts <jamesrcounts@outlook.com>
2020-02-11 07:31:07 -0800

    Download microscanner

--- infrastructure/versions.tf
+++ infrastructure/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 0.12"
   backend "azurerm" {
     resource_group_name  = "terraformrg"
-    storage_account_name = "terraformde3fb0d9c9a7413"
+    storage_account_name = "terraform0fc88c88375d45c"
     container_name       = "terraform"
     key                  = "container-pipelines-demo.tfstate"
   }
