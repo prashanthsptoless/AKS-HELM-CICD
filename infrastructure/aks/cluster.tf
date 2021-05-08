5906300b4dd6c256867a348152d8ba0f4a62d190
Jim Counts <jamesrcounts@outlook.com>
2021-05-08 22:04:28 +0000

    terraform init

--- infrastructure/aks/cluster.tf
+++ infrastructure/aks/cluster.tf
@@ -31,6 +31,6 @@ resource "azurerm_kubernetes_cluster" "main" {
 }
 
 data "azurerm_kubernetes_service_versions" "current" {
-  location = local.resource_group.location
+  location       = local.resource_group.location
   version_prefix = "1.15"
 }
