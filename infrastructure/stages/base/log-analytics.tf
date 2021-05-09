fe2ef30f6df46028ec338a75fad5f443160b8bab
Jim Counts <jamesrcounts@outlook.com>
2021-05-09 17:10:25 +0000

    Deploy log analytics to base

--- infrastructure/stages/base/log-analytics.tf
+++ infrastructure/stages/base/log-analytics.tf
@@ -1,5 +1,5 @@
 resource "azurerm_log_analytics_workspace" "main" {
-  location            = local.resource_location
+  location            = data.azurerm_resource_group.rg.location
   name                = "la-${local.instance_id}"
   resource_group_name = data.azurerm_resource_group.rg.name
   retention_in_days   = 30
