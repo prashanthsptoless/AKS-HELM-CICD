40dc24a7c79e1a875c5be5061062cead053b4f1a
Jim Counts <jamesrcounts@outlook.com>
2021-05-09 22:05:50 +0000

    Create kubernetes module

---
+++ infrastructure/modules/kubernetes/namespace-apps.tf
@@ -0,0 +1,23 @@
+resource "kubernetes_namespace" "apps" {
+  metadata {
+    name = "apps"
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "view" {
+  metadata {
+    name = "default-view"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "view"
+  }
+
+  subject {
+    kind      = "ServiceAccount"
+    name      = "default"
+    namespace = "apps"
+  }
+}
---
+++ infrastructure/modules/kubernetes/versions.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_version = ">= 0.15"
+
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.1"
+    }
+  }
+}
