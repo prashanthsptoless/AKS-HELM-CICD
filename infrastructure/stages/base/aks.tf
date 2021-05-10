4bbe15249835b5f4c8460e44c3bb78231c513716
Jim Counts <jamesrcounts@outlook.com>
2021-05-10 01:00:37 +0000

    Add prd

--- infrastructure/stages/base/aks.tf
+++ infrastructure/stages/base/aks.tf
@@ -6,3 +6,12 @@ module "aks_dev" {
   log_analytics_workspace = azurerm_log_analytics_workspace.main
   resource_group          = data.azurerm_resource_group.rg
 }
+
+module "aks_prd" {
+  source = "github.com/jamesrcounts/phippyandfriends.git//infrastructure/modules/aks?ref=azureaiconf.6.21"
+
+  environment             = "prd"
+  instance_id             = local.instance_id
+  log_analytics_workspace = azurerm_log_analytics_workspace.main
+  resource_group          = data.azurerm_resource_group.rg
+}
