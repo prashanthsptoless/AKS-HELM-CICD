1ff72577a77a084922941c4460a74790ad6df06a
Jim Counts <jamesrcounts@outlook.com>
2021-05-14 02:48:08 +0000

    Add container insights solution

--- infrastructure/stages/base/log-analytics.tf
+++ infrastructure/stages/base/log-analytics.tf
@@ -6,3 +6,17 @@ resource "azurerm_log_analytics_workspace" "main" {
   sku                 = "PerGB2018"
   tags                = data.azurerm_resource_group.rg.tags
 }
+
+resource "azurerm_log_analytics_solution" "insights" {
+  solution_name         = "ContainerInsights"
+  location              = data.azurerm_resource_group.rg.location
+  resource_group_name   = data.azurerm_resource_group.rg.name
+  workspace_resource_id = azurerm_log_analytics_workspace.main.id
+  workspace_name        = azurerm_log_analytics_workspace.main.name
+  tags = data.azurerm_resource_group.rg.tags
+
+  plan {
+    publisher = "Microsoft"
+    product   = "OMSGallery/ContainerInsights"
+  }
+}
