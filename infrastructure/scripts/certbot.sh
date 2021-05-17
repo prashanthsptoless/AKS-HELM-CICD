bbb62808fead94f373dc6dc0b6305260f1dfc50c
Jim Counts <jamesrcounts@outlook.com>
2021-05-17 03:17:02 +0000

    I really should parameterize the domain

--- infrastructure/scripts/certbot.sh
+++ infrastructure/scripts/certbot.sh
@@ -8,8 +8,8 @@ certbot --version
 mkdir -p ~/certbot/{config,work,log}
 
 # Get certificates using Route53 for validation
-DOMAIN="boss-crawdad-dev.jamesrcounts.com"
-#certbot certonly --logs-dir ~/certbot/log --config-dir ~/certbot/config --work-dir ~/certbot/work --dns-route53 -d ${DOMAIN}
+DOMAIN="boss-crawdad-prd.jamesrcounts.com"
+certbot certonly --logs-dir ~/certbot/log --config-dir ~/certbot/config --work-dir ~/certbot/work --dns-route53 -d ${DOMAIN}
 
 # PFX
 openssl pkcs12 -export -inkey ~/certbot/config/live/${DOMAIN}/privkey.pem -in ~/certbot/config/live/${DOMAIN}/cert.pem -out ${DOMAIN}.pfx
