5b7e07bd9cf6f336cf80657ea734dfb18302688a
Jim Counts <jamesrcounts@outlook.com>
2021-05-17 00:46:10 +0000

    Add vnet diagnostics

--- infrastructure/modules/aks/diagnostics.tf
+++ infrastructure/modules/aks/diagnostics.tf
@@ -4,6 +4,7 @@ module "diagnostics" {
   log_analytics_workspace_id = var.log_analytics_workspace.id
 
   monitored_services = {
-    aks = azurerm_kubernetes_cluster.aks.id
+    aks  = azurerm_kubernetes_cluster.aks.id
+    vnet = azurerm_virtual_network.vnet.id
   }
 }
