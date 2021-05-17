6c2443d693f155b562bd43ed2f92df0614b95ff9
Jim Counts <jamesrcounts@outlook.com>
2021-05-17 00:16:27 +0000

    Update principal ID

--- infrastructure/modules/aks/resource-group.tf
+++ infrastructure/modules/aks/resource-group.tf
@@ -1,6 +1,6 @@
 # Assign roles for resources in the resource group.
 resource "azurerm_role_assignment" "network_contributor" {
-  principal_id         = azurerm_user_assigned_identity.aks.id
+  principal_id         = azurerm_user_assigned_identity.aks.object_id
   role_definition_name = "Network Contributor"
   scope                = local.resource_group.id
 }
