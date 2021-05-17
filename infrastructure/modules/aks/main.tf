8262e07f6ddd4df9d14ddceed14a9aa41f6baeb0
Jim Counts <jamesrcounts@outlook.com>
2021-05-17 00:55:38 +0000

    Add UAI for AGW

--- infrastructure/modules/aks/main.tf
+++ infrastructure/modules/aks/main.tf
@@ -1,7 +1,8 @@
 locals {
-  aks_cluster_name        = "aks-${local.instance_id}-${var.environment}"
-  aks_node_labels         = { for k, v in var.resource_group.tags : k => replace(v, "/", ".") }
-  instance_id             = var.instance_id
-  log_analytics_workspace = var.log_analytics_workspace
-  resource_group          = var.resource_group
+  aks_cluster_name         = "aks-${local.instance_id}-${var.environment}"
+  aks_node_labels          = { for k, v in var.resource_group.tags : k => replace(v, "/", ".") }
+  application_gateway_name = "agw-${local.instance_id}-${var.environment}"
+  instance_id              = var.instance_id
+  log_analytics_workspace  = var.log_analytics_workspace
+  resource_group           = var.resource_group
 }
