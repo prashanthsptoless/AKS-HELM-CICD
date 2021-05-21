2cf77aa0500d32a3284362b25b8e1394f48d85a4
Jim Counts <jamesrcounts@outlook.com>
2021-05-21 17:12:16 +0000

    Add cluster principal as ACR puller

--- infrastructure/stages/base/main.tf
+++ infrastructure/stages/base/main.tf
@@ -2,6 +2,7 @@ locals {
   instance_id = data.azurerm_resource_group.rg.tags["instance_id"]
 
   acr_pullers = {
-    aks_dev = module.aks_dev.kubelet_identity_object_id
+    aks_dev         = module.aks_dev.kubelet_identity_object_id
+    aks_dev_cluster = module.aks_dev.cluster_identity_principal_id
   }
 }
