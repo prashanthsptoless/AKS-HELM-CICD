b583e67b42e7e0e544e54d34a79b311fa6fb826e
Jim Counts <jamesrcounts@outlook.com>
2021-05-21 17:50:51 +0000

    Assign ACR pull to the correct resource group

--- infrastructure/stages/base/resource-group.tf
+++ infrastructure/stages/base/resource-group.tf
@@ -3,11 +3,16 @@ data "azurerm_resource_group" "rg" {
   name = var.resource_group_name
 }
 
+# And the backend
+data "azurerm_resource_group" "backend" {
+  name = var.backend_resource_group_name
+}
+
 # Assign roles for resources in the resource group.
 resource "azurerm_role_assignment" "acr_pull" {
   for_each = local.acr_pullers
 
   principal_id         = each.value
   role_definition_name = "AcrPull"
-  scope                = data.azurerm_resource_group.rg.id
+  scope                = data.azurerm_resource_group.backend.id
 }
