9de8176e96cbbf4e447de3476e3e7a9676a52baa
Jim Counts <jamesrcounts@outlook.com>
2021-05-21 17:08:46 +0000

    Add cluster id output

--- infrastructure/modules/aks/outputs.tf
+++ infrastructure/modules/aks/outputs.tf
@@ -1,3 +1,7 @@
+output "cluster_identity_principal_id" {
+  value = azurerm_kubernetes_cluster.aks.identity.0.principal_id
+}
+
 output "kubelet_identity_object_id" {
   value = azurerm_kubernetes_cluster.aks.kubelet_identity.0.object_id
 }
