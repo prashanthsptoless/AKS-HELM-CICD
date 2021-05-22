d85fa2e77c162bd3f677e496ff6e170eb03191d1
Jim Counts <jamesrcounts@outlook.com>
2021-05-22 04:04:54 +0000

    Switch to kublet identity

--- infrastructure/stages/base/main.tf
+++ infrastructure/stages/base/main.tf
@@ -2,7 +2,7 @@ locals {
   instance_id = data.azurerm_resource_group.rg.tags["instance_id"]
 
   acr_pullers = {
-    aks_dev_cluster = module.aks_dev.cluster_identity_principal_id
-    aks_prd_cluster = module.aks_prd.cluster_identity_principal_id
+    aks_dev_cluster = module.aks_dev.kubelet_identity_object_id
+    aks_prd_cluster = module.aks_prd.kubelet_identity_object_id
   }
 }
