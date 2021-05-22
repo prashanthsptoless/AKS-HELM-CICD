c91890c57e3e61ba0701ebd069d610d5ec767a71
Jim Counts <jamesrcounts@outlook.com>
2021-05-22 13:25:58 +0000

    Simplify AGW to match docs.

--- infrastructure/modules/aks/application-gateway-identity.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_user_assigned_identity" "agw" {
-  location            = local.resource_group.location
-  name                = "uai-${local.application_gateway_name}"
-  resource_group_name = local.resource_group.name
-  tags                = var.resource_group.tags
-}
