01cf2e857c9218d4f67de864b65c68afd829bdff
Jim Counts <jamesrcounts@outlook.com>
2021-05-22 23:32:42 +0000

    Replace password lookup with certificate lookup.

--- infrastructure/stages/base/aks.tf
+++ infrastructure/stages/base/aks.tf
@@ -1,21 +1,21 @@
 module "aks_dev" {
   source = "github.com/jamesrcounts/phippyandfriends.git//infrastructure/modules/aks?ref=azureaiconf.6.21"
 
-  cert_password           = data.azurerm_key_vault_secret.password["dev"].value
-  certs_path              = "${path.module}/certs"
-  environment             = "dev"
-  instance_id             = local.instance_id
-  log_analytics_workspace = azurerm_log_analytics_workspace.main
-  resource_group          = data.azurerm_resource_group.rg
+  cert_secret_id               = data.azurerm_key_vault_certificate.certificate["dev"].secret_id
+  environment                  = "dev"
+  instance_id                  = local.instance_id
+  log_analytics_workspace      = azurerm_log_analytics_workspace.main
+  resource_group               = data.azurerm_resource_group.rg
+  configuration_resource_group = data.azurerm_resource_group.config
 }
 
 module "aks_prd" {
   source = "github.com/jamesrcounts/phippyandfriends.git//infrastructure/modules/aks?ref=azureaiconf.6.21"
 
-  cert_password           = data.azurerm_key_vault_secret.password["prd"].value
-  certs_path              = "${path.module}/certs"
-  environment             = "prd"
-  instance_id             = local.instance_id
-  log_analytics_workspace = azurerm_log_analytics_workspace.main
-  resource_group          = data.azurerm_resource_group.rg
+  cert_secret_id               = data.azurerm_key_vault_certificate.certificate["dev"].secret_id
+  environment                  = "prd"
+  instance_id                  = local.instance_id
+  log_analytics_workspace      = azurerm_log_analytics_workspace.main
+  resource_group               = data.azurerm_resource_group.rg
+  configuration_resource_group = data.azurerm_resource_group.config
 }
--- infrastructure/stages/base/sercrets.tf
+++ infrastructure/stages/base/sercrets.tf
@@ -1,12 +1,16 @@
+data "azurerm_resource_group" "config" {
+  name = var.backend_resource_group_name
+}
+
 data "azurerm_key_vault" "config" {
   name                = var.configuration_keyvault_name
   resource_group_name = var.backend_resource_group_name
 }
 
-data "azurerm_key_vault_secret" "password" {
+data "azurerm_key_vault_certificate" "certificate" {
   for_each = {
-    dev = "cert-password-dev"
-    prd = "cert-password-prd"
+    dev = "boss-crawdad-dev-jamesrcounts-com"
+    prd = "boss-crawdad-prd-jamesrcounts-com"
   }
 
   name         = each.value
