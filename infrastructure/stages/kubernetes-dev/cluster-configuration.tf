77253a7bec419e82d9d86c763c113c3ee0bc60a3
Jim Counts <jamesrcounts@outlook.com>
2021-05-22 16:31:55 +0000

    Lookup aks subnet to configure nginx

--- infrastructure/stages/kubernetes-dev/cluster-configuration.tf
+++ infrastructure/stages/kubernetes-dev/cluster-configuration.tf
@@ -1,3 +1,5 @@
 module "aks" {
   source = "github.com/jamesrcounts/phippyandfriends.git//infrastructure/modules/kubernetes?ref=azureaiconf.6.21"
+
+  aks_subnet_cidr = data.azurerm_subnet.aks.address_prefixes.0
 }
--- infrastructure/stages/kubernetes-dev/variables.tf
+++ infrastructure/stages/kubernetes-dev/variables.tf
@@ -3,6 +3,11 @@ variable "aks_cluster_name" {
   type        = string
 }
 
+variable "aks_vnet_name" {
+  description = "(Required) The name for the vnet containing the subnet configured for AKS nodes."
+  type        = string
+}
+
 variable "resource_group_name" {
   description = "(Required) The name of the resource group for this project."
   type        = string
