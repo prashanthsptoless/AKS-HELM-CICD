1485aba0dac4db06f0d5373df7215520f8651b9d
Jim Counts <jamesrcounts@outlook.com>
2021-05-23 16:52:25 +0000

    Remove broken data resource

--- infrastructure/modules/kubernetes/configuration.tf
+++ infrastructure/modules/kubernetes/configuration.tf
@@ -7,11 +7,6 @@ data "azurerm_key_vault" "config" {
   resource_group_name = var.backend_resource_group_name
 }
 
-data "azurerm_key_vault_certificate_data" "tls" {
-  key_vault_id = data.azurerm_key_vault.config.id
-  name         = var.certificate_name
-}
-
 data "azurerm_key_vault_secret" "tls" {
   key_vault_id = data.azurerm_key_vault.config.id
   name         = var.certificate_name
