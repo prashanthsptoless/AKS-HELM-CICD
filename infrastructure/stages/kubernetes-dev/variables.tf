dae3bd22ac7f1595b486daaa5b1e179c669c3950
Jim Counts <jamesrcounts@outlook.com>
2021-05-23 13:41:45 +0000

    Configure backend keyvault in kube-dev

--- infrastructure/stages/kubernetes-dev/variables.tf
+++ infrastructure/stages/kubernetes-dev/variables.tf
@@ -12,3 +12,13 @@ variable "resource_group_name" {
   description = "(Required) The name of the resource group for this project."
   type        = string
 }
+
+variable "backend_resource_group_name" {
+  description = "(Required) The name of the resource group containing backend resources for this project."
+  type        = string
+}
+
+variable "configuration_keyvault_name" {
+  description = "(Required) The name of the key vault containing configuration data."
+  type        = string
+}
