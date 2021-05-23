642f32754f75d3b57a1c07821bc1f1df532b3bd4
Jim Counts <jamesrcounts@outlook.com>
2021-05-23 14:09:22 +0000

    Read cert data from secret

--- infrastructure/modules/kubernetes/secret-tls.tf
+++ infrastructure/modules/kubernetes/secret-tls.tf
@@ -2,11 +2,12 @@ resource "kubernetes_secret" "tls" {
   type = "kubernetes.io/tls"
 
   metadata {
-    name = "ingress-tls"
+    name      = "ingress-tls"
+    namespace = kubernetes_namespace.apps.metadata.0.name
   }
 
   data = {
-    "tls.crt" = data.azurerm_key_vault_certificate.certificate.certificate_data_base64
-    "tls.key" = data.azurerm_key_vault_certificate.certificate.certificate_data_base64
+    "tls.crt" = data.azurerm_key_vault_secret.certificate_data.value
+    "tls.key" = data.azurerm_key_vault_secret.certificate_data.value
   }
 }
