4e708bed6b7472c41ac01cee2f732ab5b431b585
Jim Counts <jamesrcounts@outlook.com>
2021-05-24 02:58:36 +0000

    configure provider

--- infrastructure/stages/kubernetes-prd/providers.tf
+++ infrastructure/stages/kubernetes-prd/providers.tf
@@ -8,3 +8,12 @@ provider "kubernetes" {
   client_key             = base64decode(local.kube_config["client_key"])
   cluster_ca_certificate = base64decode(local.kube_config["cluster_ca_certificate"])
 }
+
+provider "helm" {
+  kubernetes {
+    host                   = local.kube_config["host"]
+    client_certificate     = base64decode(local.kube_config["client_certificate"])
+    client_key             = base64decode(local.kube_config["client_key"])
+    cluster_ca_certificate = base64decode(local.kube_config["cluster_ca_certificate"])
+  }
+}l;;l,,.
