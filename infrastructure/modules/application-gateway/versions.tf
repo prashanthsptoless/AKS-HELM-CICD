0a02327005679376562c3b67babc204139fabaf1
Jim Counts <jamesrcounts@outlook.com>
2021-05-24 13:47:31 +0000

    Add route53 zone lookup

--- infrastructure/modules/application-gateway/versions.tf
+++ infrastructure/modules/application-gateway/versions.tf
@@ -2,6 +2,11 @@ terraform {
   required_version = ">= 0.15"
 
   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3"
+    }
+
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 2"
